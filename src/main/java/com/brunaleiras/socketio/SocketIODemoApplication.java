06637a2548dd330429192b67bb16caac18488406
Bruna Leiras <bruna-leiras@ectec10nl.culturainglesa.int>
2021-03-12 19:05:17 -0300

    adding files with full project

---
+++ src/main/java/com/brunaleiras/socketio/SocketIODemoApplication.java
@@ -0,0 +1,30 @@
+package com.brunaleiras.socketio;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SocketIODemoApplication {
+
+    @Value("${websocket-server.host}")
+    private String host;
+
+    @Value("${websocket-server.port}")
+    private Integer port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        Configuration config = new Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        return new SocketIOServer(config);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketIODemoApplication.class, args);
+    }
+}
